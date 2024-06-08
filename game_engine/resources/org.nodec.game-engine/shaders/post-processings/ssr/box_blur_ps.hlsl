00b03f7277722bd017c0f1fef253bab7b3022f44
IOE <fivetwothreesix@gmail.com>
2024-06-08 16:43:29 +0900

    chore: Add box-blur shader

--- game_engine/resources/org.nodec.game-engine/shaders/post-processings/ssr/box_blur_ps.hlsl
+++ game_engine/resources/org.nodec.game-engine/shaders/post-processings/ssr/box_blur_ps.hlsl
@@ -4,29 +4,40 @@ struct PSOut {
     float4 color : SV_Target0;
 };
 
-PSOut PSMain(V2P input) {
+float4 PSMain(V2P input) : SV_TARGET {
     PSOut output;
+
+    float tex_width, tex_height;
+    tex_reflection_color.GetDimensions(tex_width, tex_height);
+
     float4 color = tex_reflection_color.Sample(sampler_tex, input.texcoord);
+    color.a = 1; // ???
+    
+    // return color;
     
     int size = 1;
     if (size <= 0) {
-        output.color = color;
-        return output;
+        return color;
+        // output.color = color;
+        // return output;
     }
 
-    int separation = 2;
+    int separation = 1;
     separation = max(separation, 1);
 
+    color = float4(0, 0, 0, 1);
     float count = 0.0;
 
-    for (int i = -size; i <- size; ++i) {
+    for (int i = -size; i <= size; ++i) {
         for (int j = -size; j <= size; ++j) {
-            color.rgb += tex_reflection_color.Sample(sampler_tex, input.texcoord + float2(i * separation, j * separation)).rgb;
+            color.rgb += tex_reflection_color.Sample(sampler_tex,
+                input.texcoord + float2(i * separation / tex_width, j * separation / tex_height)).rgb;
             count += 1.0;
         }
     }
 
     color.rgb /= count;
-    output.color = color;
-    return output;
+    return color;
+    // output.color = color;
+    // return output;
 }
--- game_engine/resources/org.nodec.game-engine/shaders/post-processings/ssr/reflection_color_ps.hlsl
+++ game_engine/resources/org.nodec.game-engine/shaders/post-processings/ssr/reflection_color_ps.hlsl
@@ -1,20 +1,30 @@
 #include "reflection_color_interface.hlsl"
 
-float4 PSMain(V2P input) : SV_TARGET {
+struct PSOut {
+    float4 color : SV_TARGET0;
+};
+
+PSOut PSMain(V2P input) {
+    PSOut output;
+
     float screen_tex_width, screen_tex_height;
     tex_screen.GetDimensions(screen_tex_width, screen_tex_height);
 
     float4 reflection_uv = tex_reflection_uv.Sample(sampler_tex, input.texcoord);
     // return float4(reflection_uv.xy, 0, 1);
     if (reflection_uv.b <= 0.0) {
-        return float4(0, 0, 0, 1);
+        output.color = float4(0, 0, 0, 1);
+        return output;
     }
     // float4 color = tex_screen.Sample(sampler_tex, input.texcoord);
     float4 color = tex_screen.Sample(sampler_tex, reflection_uv);
     float alpha = clamp(reflection_uv.b, 0, 1);
     // alpha = 1;
 
-    return float4(lerp(float3(0,0,0), color.rgb, alpha), 1);
+    // return float4(lerp(float3(0,0,0), color.rgb, alpha), 1);
+    
+    output.color = float4(lerp(float3(0,0,0), color.rgb, alpha), 1);
+    return output;
     // return float4(color.rgb, 1);
     // return float4(reflection_uv.xy, 0, 1);
 }
