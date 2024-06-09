8974a2c3b49ed326fac4e26e471161d22e011eec
IOE <fivetwothreesix@gmail.com>
2024-06-09 18:19:52 +0900

    chore: Adjust color

--- game_engine/resources/org.nodec.game-engine/shaders/post-processings/ssr/combine_interface.hlsl
+++ game_engine/resources/org.nodec.game-engine/shaders/post-processings/ssr/combine_interface.hlsl
@@ -2,7 +2,9 @@
 
 Texture2D tex_screen : register(t0);
 Texture2D tex_reflection_color : register(t1);
-Texture2D tex_normal : register(t2);
+Texture2D tex_mat_props : register(t2);
+Texture2D tex_normal : register(t3);
+Texture2D tex_depth : register(t4);
 SamplerState sampler_tex : register(s0);
 
 struct V2P {
